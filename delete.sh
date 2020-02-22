eb5a7306b37b617a16cf4dc8c0dc02751d927378
juanra <JuanRamon.AlonsoGarcia@zooplus.com>
2020-02-23 00:29:13 +0100

    commit of deleted files

--- delete.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation delete-stack \
---stack-name $1 \
---region=us-west-2 \
---profile=jra-udacity
--- list.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation list-stacks \
---stack-status-filter $1 \
---region=us-west-2 \
---profile=jra-udacity
